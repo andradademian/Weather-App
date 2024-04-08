ec53fb13c3776e3a9766c61789ca75d49dc6c31b
Andrada <andradademian@gmail.com>
2024-04-08 18:53:07 +0300

    Added first batch of files

---
+++ Weather App/WeatherApp/src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    production: false,
+    weatherApiBaseUrl: 'https://open-weather-map27.p.rapidapi.com/weather', 
+    XRapidAPIHostHeaderName:'X-RapidAPI-Host', 
+    XRapidAPIHostHeaderValue: 'open-weather-map27.p.rapidapi.com',
+    XRapidAPIKeyHeaderName: "X-RapidAPI-Key",
+    XRapidAPIKeyHeaderValue:"b627b9242fmshc06befdadfa786dp16083fjsn5be09ec91988"
+
+    
+};
